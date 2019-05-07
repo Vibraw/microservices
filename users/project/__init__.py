ab0307e8c8368502ac6a40ec55df1d8675f5afe4
wickieonya <wickieonya@gmail.com>
2019-05-07 16:54:57 +0300

    setup

---
+++ users/project/__init__.py
@@ -0,0 +1,25 @@
+
+from flask import Flask, jsonify
+from flask_restful import Resource, Api
+
+# instantiate the app
+app = Flask(__name__)
+
+api = Api(app)
+
+# set config
+app.config.from_object('project.config.DevelopmentConfig')
+
+
+class UsersPing(Resource):
+    def get(self):
+        return {
+            'status': 'success',
+            'message': 'pong!',
+            'status' : 'false'
+        }
+
+api.add_resource(UsersPing, '/users/ping')
+
+if __name__ == '__main__':
+    app.run(debug=True)
